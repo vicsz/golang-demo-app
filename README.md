e875fc5f3a9fa9ed00f156599cf9eb3d9afb53e7
Victor Sz <vicsz@users.noreply.github.com>
2021-04-09 12:07:48 -0400

    Add Webapp functionality

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# Go Lang Demo App
+# Go Lang Demo WebApp
 
 ![](https://github.com/vicsz/golang-demo-app/workflows/Golang%20CI%20Build/badge.svg
 )
 
-Sample Golang demo app. 
+Sample Golang demo webapp. 
+
+## Demo Requirements
+
+### 1 - Golang installed on the command line 
+
+## Demo Steps
+
+### 1 - Run the app
+
+```sh
+go run app.go
+```
+
+### 2 - Verify the app
+
+Browse to *localhost:8080*
--- app.go
+++ app.go
@@ -1,7 +1,22 @@
 package main
-  
-import "fmt"
-  
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Helloworld!")
+
+    fmt.Println("Endpoint Hit: Helloworld")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", helloWorld)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-    fmt.Println("Hello world!!")
+    handleRequests()
 }
